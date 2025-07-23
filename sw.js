37e79ccd5b56ca6cfc2c5c92944512b80f9584d4
ivanclas <111611656+ivanclas@users.noreply.github.com>
2025-07-23 09:07:26 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -4,20 +4,20 @@ self.addEventListener('install', (e) => {
     caches.open('my-cache').then((cache) => {
       return cache.addAll([
         '/',
-        '/index.html',
-         '/condiciones.html',
-         '/formulario.html',
-         '/login.html',
-         '/menu.html',
-         '/MisCarpetas.html',
-         '/nuevo.html',
-         '/privacidad.html',
-         '/verGaleria.html',
-         '/verGaleria2.html',
-         '/verGaleria3.html',
-        '/script.js',
-        '/icons/iconGaleria.png',
-        '/manifest.json'
+        'index.html',
+         'condiciones.html',
+         'formulario.html',
+         'login.html',
+         'menu.html',
+         'MisCarpetas.html',
+         'nuevo.html',
+         'privacidad.html',
+         'verGaleria.html',
+         'verGaleria2.html',
+         'verGaleria3.html',
+        'script.js',
+        'iconGaleria.png',
+        'manifest.json'
       ]);
     })
   );
